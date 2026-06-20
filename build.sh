194334da64f5d265068cb1e8af7f129d4be41df3
root <root@ip-172-31-20-86.ec2.internal>
2026-06-20 14:35:34 +0000

    Fix container stop and remove commands

--- build.sh
+++ build.sh
@@ -4,15 +4,15 @@
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASS
 
 #stopping existing container:
-docker stop react
-docker rm react
+docker stop react || true
+docker rm react || true
 
 #building a image:
 docker build -t react-ci/cd .
 
 #running a container from the created image:
-docker run -d -it --name react -p 80:80 react-ci/cd
+docker run -d --name react -p 80:80 react-ci/cd
 
 #pushing the image to dockerhub:
-docker tag react-ci/cd naveen712/react-app:ci-cd
-docker push naveen712/react-app:ci-cd
+docker tag react-ci/cd roshid76/react-app:ci-cd
+docker push roshid76/react-app:ci-cd
